371ba0b27cfd6c6026a6fe2d339dcef064c46f1f
egeerdogan <ege@erdogan.dev>
2020-11-09 19:49:05 +0300

    09.11 solutions

--- 11_09/problem_07.py
+++ 11_09/problem_07.py
@@ -5,3 +5,17 @@ November 9-10-11, 2020
 -- Problem 7 --
 Given a positive integer n, assign True to is_prime if n has no factors other than 1 and itself. (Remember, m is a factor of n if m divides n evenly.)
 '''
+import math
+
+n = 26
+is_prime = True
+
+if n > 2 and n % 2 == 0:
+    is_prime = False
+else:
+    for i in range(3, int(math.sqrt(n)), 2):
+        if n % i == 0:
+            is_prime = False
+            break
+
+print(is_prime)
